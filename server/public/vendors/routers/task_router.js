c9bb603f405a0938e5c64379333a8174094ba8af
Michael Shaleen <shaleenmichael@protonmail.com>
2021-02-12 09:06:49 -0600

    postico set up and starting table made

--- server/public/vendors/routers/task_router.js
+++ server/public/vendors/routers/task_router.js
@@ -8,3 +8,13 @@ router.get('/', (req, res) => {
   ("tasks" "Due Date)
   VALUES
   ('$1, $2)`};
+
+
+  let queryArg = [
+    req.body.name, //$1
+    req.body.dueDate, //$2
+  ];
+
+
+  pool
+    .query(queryString, queryArg)// plugged into function
